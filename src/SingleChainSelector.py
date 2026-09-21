5167fa84d5b59c52107ddb8308645d9cbaa64487
Joe Hoye Dow <luxunisgod123@gmail.com>
2026-09-21 21:24:36 +0800

    update pyproject.toml, and update license

--- src/SingleChainSelector.py
+++ src/SingleChainSelector.py
@@ -6,6 +6,9 @@
 # choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
 # Please see the LICENSE file that should have been included as part of this
 # package.
+#
+# Modifications for pdb-select: Copyright (C) 2026, Joe Hoye Dow
+# Licensed under the BSD 3-Clause License (see LICENSE).
 
 """Code for chopping up (dicing) a structure.
 
